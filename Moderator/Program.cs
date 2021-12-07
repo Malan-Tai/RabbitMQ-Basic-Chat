4a3db281763c9f59bddaf1767d0709f6b71a115d
Julien DOCK <dock.julien@orange.fr>
2021-12-07 15:44:01 +0100

    initial commit

---
+++ Moderator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Moderator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
